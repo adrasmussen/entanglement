436985988cf2951dc55b5ac43dd83f92dd12f109
alex <no@reply>
2025-04-29 01:06:28 -0700

    rework awaitcache to avoid holding dashmap references over an await boundary

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,9 @@
-use std::{cmp::Eq, future::Future, hash::Hash, fmt::Debug};
+use std::{cmp::Eq, fmt::Debug, future::Future, hash::Hash, sync::Arc};
 
+use anyhow::Result;
+use async_cell::sync::AsyncCell;
 use dashmap::{mapref::entry::Entry, DashMap};
+use tracing::{error, instrument};
 
 pub mod auth;
 pub mod config;
@@ -26,30 +29,54 @@ pub const GROUP_REGEX: &str = r"^[a-zA-Z0-9_.-]{1,64}$";
 // note that there have historically been some issues with DashMap and holding references across
 // await boundaries, but they have largely been cleared up
 #[derive(Debug)]
-pub struct AwaitCache<K: Clone + Debug + Eq + Hash , V: Clone + Debug> {
-    items: DashMap<K, V>,
+pub struct AwaitCache<K: Clone + Debug + Eq + Hash, V: Clone + Debug> {
+    items: DashMap<K, Arc<AsyncCell<Option<V>>>>,
 }
 
-impl<K: Clone + Debug + Eq + Hash , V: Clone + Debug> AwaitCache<K, V> {
+impl<K: Clone + Debug + Eq + Hash, V: Clone + Debug> AwaitCache<K, V> {
     pub fn new() -> Self {
         AwaitCache {
             items: DashMap::new(),
         }
     }
 
-    pub async fn perhaps<Fut: Future<Output = anyhow::Result<V>>>(
-        &self,
-        key: K,
-        init: Fut,
-    ) -> anyhow::Result<V> {
-        let val = match self.items.entry(key) {
-            Entry::Occupied(entry) => entry.get().clone(),
+    #[instrument(skip(self, init))]
+    pub async fn perhaps<Fut: Future<Output = Result<V>>>(&self, key: K, init: Fut) -> Result<V> {
+        // since we need to determine if a thread should initialize the value in the map while
+        // holding the lock, we can't use the native get() -- it would need to return Mutex<Option<>>
+        // instead of the reverse
+        let (cell, set) = match self.items.entry(key.clone()) {
+            Entry::Occupied(entry) => (entry.get().clone(), false),
             Entry::Vacant(entry) => {
-                let val = init.await?;
+                let cell = Arc::new(AsyncCell::new());
 
-                entry.insert(val.clone());
-                val
+                entry.insert(cell.clone());
+                (cell, true)
             }
+        };
+
+        // attempt to initialize the cell
+        //
+        // if this fails, we need to set the value to None (signalling to any listeners that this
+        // attempt failed), remove the now-stale cell, and return an error to the caller
+        //
+        // we could instead have the Entry::Occupied match arm check if the value is None
+        let val = if set {
+            let val = match init.await {
+                Ok(v) => v,
+                Err(err) => {
+                    error!("error during cell initialization");
+                    cell.set(None);
+                    self.remove(&key);
+                    return Err(anyhow::Error::from(err));
+                }
+            };
+
+            cell.set(Some(val.clone()));
+
+            val
+        } else {
+            cell.get().await.ok_or_else(|| anyhow::Error::msg("cell initializing thread failed"))?
         };
 
         Ok(val)
